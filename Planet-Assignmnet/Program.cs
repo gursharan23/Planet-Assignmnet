39f7e3eac0f40565635092930b88842527e1daae
gursharan23 <iamgursharan@gmail.com>
2017-07-26 21:31:45 -0400

    Added WaitForAnyKey method

--- Planet-Assignmnet/Program.cs
+++ Planet-Assignmnet/Program.cs
@@ -2,16 +2,35 @@
 using System.Collections.Generic;
 /*
  * Name : Gursharan Singh
+ * Student Number : 300931676
  * Date : 23 July, 2017
  * Description : This is the main driver class
- * Version: 0.1 Created  program class
+ * Version: 0.2 Added WaitForAnyKey method
  */
 namespace Planet_Assignmnet
 {
-    internal class Program
+    /// <summary>
+    /// This is the driver class
+    /// </summary>
+   internal class Program
     {
         public static void Main(string[] args)
         {
+            GiantPlanet planet=new GiantPlanet("Jupiter",100.25,150,"Ice");
+            Console.WriteLine(planet.ToString());
+            WaitForAnyKey();
+
+        }
+        //Private Methods
+        /// <summary>
+        /// This is a private method with no parameters 
+        /// This method is used to close the cancel
+        /// </summary>
+        private static void WaitForAnyKey()
+        {
+            Console.WriteLine("  Press any key to exit");
+            Console.ReadLine();
+            Environment.Exit(0);
         }
     }
 }
